4999fc042040174e458220f7c6800601acc17377
josecv <jose.cortesvarela@alum.utoronto.ca>
2021-02-16 13:36:12 -0500

    Simple example counting python garbage collections

--- example/server.py
+++ example/server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3.9
 
+import gc
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +10,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    # Generate some allocations so there's something to gc
+    memory = []
+    for _ in range(random.randint(5, 10)):
+        memory.append(bytearray(random.randint(10, 20)))
+    del memory
+    if random.randint(0, 4) == 0:
+        # Explicitly garbage collect approx every 5 requests
+        gc.collect()
     return 'Hello from example app!'
